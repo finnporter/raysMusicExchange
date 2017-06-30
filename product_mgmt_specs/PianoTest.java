9a4b610bda78282354fac162f978b9bc612d898b
finnporter <iamfinnporter@gmail.com>
2017-06-30 19:04:03 +0100

    test Piano has all properties

--- product_mgmt_specs/PianoTest.java
+++ product_mgmt_specs/PianoTest.java
@@ -1,3 +1,54 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import product_mgmt.*;
+
+public class PianoTest {
+
+  Piano piano01;
+
+  @Before
+  public void before() {
+    piano01 = new Piano("black", "grand piano", "grand", 10000, 12000, "Bösendorfer", "Grand Piano 170", 88);
+    //String colour, String type, String size, Double buyPrice, Double sellPrice, String brand, String, model, int numberOfStrings
+  }
+
+  @Test
+  public void testHasColour() {
+    assertEquals("black", piano01.getColour());
+  }
+
+  @Test
+  public void testHasType() {
+    assertEquals("grand piano", piano01.getType());
+  }
+
+  @Test
+  public void testHasSize() {
+    assertEquals("grand", piano01.getSize());
+  }
+
+  @Test
+  public void testHasBuyPrize() {
+    assertEquals(10000, piano01.getBuyPrice());
+  }
+
+  @Test
+  public void testHasSellPrize() {
+    assertEquals(12000, piano01.getSellPrice());
+  }
+
+  @Test
+  public void testHasBrand() {
+    assertEquals("Bösendorfer", piano01.getBrand());
+  }
+
+  @Test
+  public void testHasModel() {
+    assertEquals("Grand Piano 170", piano01.getModel());
+  }
+
+  @Test
+  public void testHasNumberOfStrings() {
+    assertEquals(88, piano01.getNumberOfKeys());
+  }
+}
