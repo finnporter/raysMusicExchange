effc93996d4b6ccf027030cd7d0ef70fa804130e
finnporter <iamfinnporter@gmail.com>
2017-07-01 09:15:52 +0100

    adds shop class

--- product_mgmt/Shop.java
+++ product_mgmt/Shop.java
@@ -1,1 +1,14 @@
 package product_mgmt;
+import behaviours.*;
+
+public class Shop {
+
+  private ArrayList<Sellable> stock;
+
+  public Shop(ArrayList<Sellable> stock) {
+    this.stock = stock;
+  }
+
+  
+
+}
